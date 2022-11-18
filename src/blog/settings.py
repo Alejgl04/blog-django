932ef0fbf643fc393db1f37c448d14e55b2c8516
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2022-11-17 20:44:59 -0500

    posts views urls and models

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templatess')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from .models import Post, Comment, PostView, Like
 # Create your views here.
+
+class PostListView( ListView ):
+  model = Post
+
+
+class PostDetailView( DetailView ):
+  model = Post
+
+
+class PostCreateView( CreateView ):
+  model = Post
+  fields = {
+    'title',
+    'content',
+    'tumbnail',
+    'author',
+    'slug'
+  }
+
+class PostUpdateView( UpdateView ):
+  model = Post
+  fields = {
+    'title',
+    'content',
+    'tumbnail',
+    'author',
+    'slug'
+  }
+
+
+class PostDeleteView( DeleteView ):
+  model = Post
+  success_url = '/'
+
+
+
+
