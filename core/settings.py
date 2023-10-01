2f1057c20bc82422effc056d4a858955d0523558
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-09-30 20:13:01 -0400

    homeView

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,7 +40,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
